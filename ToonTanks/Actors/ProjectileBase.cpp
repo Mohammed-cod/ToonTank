d90f61c2a119f6a5cfbb900da67c77c111d2a024
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2022-05-17 02:27:11 +0300

    commit

--- ToonTanks/Actors/ProjectileBase.cpp
+++ ToonTanks/Actors/ProjectileBase.cpp
@@ -28,10 +28,7 @@ void AProjectileBase::BeginPlay()
 	Super::BeginPlay();
 
 }
-
-
-
-
+// testing 
 void AProjectileBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	// Try to get a reference to the owning class
